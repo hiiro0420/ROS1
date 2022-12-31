45466ed11333499502b5d2866251d333c5bbb00a
hiiro0420 <180442111@ccalumni.meijo-u.ac.jp>
2022-12-31 22:04:46 +0900

    注釈を追加

--- rospy_service/src/client.py
+++ rospy_service/src/client.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 
 import rospy
+
+# 使用するsrvファイルを定義(重要)
 from rospy_service.srv import plus
 
+# ノードの名前を定義
 rospy.init_node("client")
 
+# Plusという名前のサービスが立ち上がるまで待機
 rospy.wait_for_service("Plus")
 
+# Plusという名前のサービスとplusという名前のsrvファイルを使用することを定義
 pub = rospy.ServiceProxy("Plus",plus)
 
+# サービスへと値を送る
 print(pub(1,4))
 
 
--- rospy_service/src/service.py
+++ rospy_service/src/service.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 import rospy
 
+# 使用するsrvファイルを定義(重要)
 from rospy_service.srv import plus, plusResponse
 
+# 関数を定義
 def callback(data):
 
     return data.x + data.y
 
 def service():
 
+    # nodeの名前を決定(なんでもよい)
     rospy.init_node("server",anonymous = False)
+
+    # serviceの名前、srv、関数を定義
     server = rospy.Service("Plus",plus,callback)
     
     rospy.spin()
