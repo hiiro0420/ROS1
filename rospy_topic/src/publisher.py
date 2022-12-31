7884efbaeae1f090cfe9d3526ac22526a640f40d
hiiro0420 <180442111@ccalumni.meijo-u.ac.jp>
2022-12-31 21:56:05 +0900

    add 注釈を追加

--- rospy_topic/src/publisher.py
+++ rospy_topic/src/publisher.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
+# rospyを使うことを宣言(pythonなら必須)
 import rospy
+# メッセージの型を宣言(重要)
 from std_msgs.msg import String
 
+# nodeの名前を決定(なんでもよい)
 rospy.init_node("publisher")
 
+# メッセージの名前や型、サイズを決定
 pub = rospy.Publisher("chatter",String,queue_size = 10)
 
+# メッセージを送る周期を決定
 r = rospy.Rate(10)
 
 while not rospy.is_shutdown():
+
+    # パブリッシュを実行
     pub.publish("hello")
     r.sleep()
--- rospy_topic/src/subscrive.py
+++ rospy_topic/src/subscrive.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
+# rospyを使うことを宣言(pythonなら必須)
 import rospy
+
+# メッセージの型を宣言(重要)
 from std_msgs.msg import String
 
+# 受け取ったメッセージを処理する関数
 def callback(data):
     print(data)
 
 def listener():
 
+    # nodeの名前を決定(なんでもよい)
     rospy.init_node("subscriber",anonymous = False)
+
+    # 受け取るメッセージの名前、型を定義し受け取ったメッセージを処理する関数まで定義
     rospy.Subscriber("chatter",String,callback)
 
     rospy.spin()
