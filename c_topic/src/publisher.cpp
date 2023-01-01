4b32ecc3cd6b11c5bdda5b9995afe781ac44e690
hiiro0420 <180442111@ccalumni.meijo-u.ac.jp>
2023-01-01 16:20:34 +0900

    add 注釈を追加

--- c_topic/src/publisher.cpp
+++ c_topic/src/publisher.cpp
@@ -1,15 +1,23 @@
 #include<ros/ros.h>
+
+// メッセージの型を決定(重要)
 #include<std_msgs/String.h>
 
 int main(int argc, char** argv){
+
+    // ノードの名前を定義
     ros::init(argc,argv,"simple_talker");
     ros::NodeHandle n;
+
+    // メッセージの型、名前、サイズを決定
     ros::Publisher ros_pub = n.advertise<std_msgs::String>("chatter",10);
     ros::Rate loop_rate(10);
 
     while(ros::ok()){
         std_msgs::String msg;
         msg.data = "hello World";
+        
+        // パブリッシュ
         ros_pub.publish(msg);
 
         ros::spinOnce();
--- c_topic/src/subscriber.cpp
+++ c_topic/src/subscriber.cpp
@@ -1,13 +1,18 @@
 #include<ros/ros.h>
 #include<std_msgs/String.h>
 
+// 関数を定義
 void callback(const std_msgs::String& msg){
     ROS_INFO("%s",msg.data.c_str());
 }
 
 int main(int argc, char** argv){
+
+    // ノードの名前を決定
     ros::init(argc,argv,"simple_listener");
     ros::NodeHandle n;
+
+    // メッセージに名前、サイズ、関数を定義
     ros::Subscriber ros_sub = n.subscribe("chatter",10,callback);
 
     ros::spin();
